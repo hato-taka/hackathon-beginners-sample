ce9a2b85f147bbcf77eb7915031930eb3a34957b
Rinasham <rinashamau@gmail.com>
2024-12-16 09:16:34 +1100

    [fix] CSSをバンドル

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template, session, flash, abort
+from flask_assets import Environment, Bundle
 from datetime import timedelta
 import hashlib
 import uuid
@@ -6,9 +7,17 @@ import re
 
 from models import User, Channel, Message
 
+
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
 app.permanent_session_lifetime = timedelta(days=30)
+
+# CSSの圧縮と結合
+assets = Environment(app)
+css_bundle = Bundle('css/*.css', filters='cssmin', output='gen/bundled.css')
+assets.register('css_all', css_bundle)
+css_bundle.build()
+
 
 
 # サインアップページの表示
@@ -196,4 +205,4 @@ def internal_server_error(error):
     return render_template('error/500.html'),500
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0", debug=True)
+        app.run(host="0.0.0.0", debug=False)
