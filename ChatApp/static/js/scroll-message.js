8a64f6dbe67b281b0870fe707739280eaa3e7d3d
Rinasham <rinashamau@gmail.com>
2024-12-19 00:09:56 +1100

    [fix/FE] スクロール修正

--- ChatApp/static/js/scroll-message.js
+++ ChatApp/static/js/scroll-message.js
@@ -2,7 +2,12 @@
 各チャンネル詳細ページ内、ページ読み込み時に自動で下までスクロールする
 */
 
-document.addEventListener("DOMContentLoaded", function () {
-  const messageArea = document.getElementById("message-area");
-  messageArea.scrollIntoView(false);
+
+const element = document.getElementById("message-area");
+const offset = 16 * window.innerHeight / 100; // 16vhを計算
+const elementBottom = element.getBoundingClientRect().bottom;
+
+window.scrollBy({
+  top: elementBottom - window.innerHeight + offset,
+  behavior: 'smooth'
 });
