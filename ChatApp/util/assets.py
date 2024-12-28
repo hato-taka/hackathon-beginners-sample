a57a7e01df7aa5d711481deb8be8e4cc622a089e
purewest-github <github@purewest.dip.jp>
2024-12-28 10:59:36 +0900

    classmethodを明示的に記述。SQLインジェクション対策。コメント追加。

--- ChatApp/util/assets.py
+++ ChatApp/util/assets.py
@@ -1,8 +1,8 @@
 from flask_assets import Environment, Bundle
 
 def bundle_css_files(app):
-  # CSSファイルのバンドル（圧縮・結合）
-  assets = Environment(app)
-  css_bundle = Bundle('css/*.css', filters='cssmin', output='gen/bundled.css')
-  assets.register('css_all', css_bundle)
-  css_bundle.build()
+   # CSSファイルのバンドル（圧縮・結合）
+   assets = Environment(app)
+   css_bundle = Bundle('css/*.css', filters='cssmin', output='gen/bundled.css')
+   assets.register('css_all', css_bundle)
+   css_bundle.build()
